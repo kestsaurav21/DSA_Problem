f5fdeec89801876e9c17305fde171665da81ce64
kestsaurav21 <kestsaurav21@outlook.com>
2024-08-26 23:22:20 +0530

    Find The Single Element

---
+++ Arrays/SingleElementInArray.cpp
@@ -0,0 +1,47 @@
+// Problem statement
+// You are given a sorted array 'arr' of positive integers of size 'n'.
+
+// It contains each number exactly twice except for one number, which occurs exactly once.
+
+// Find the number that occurs exactly once.
+
+
+
+// Example :
+// Input: ‘arr’ = {1, 1, 2, 3, 3, 4, 4}.
+
+// Output: 2
+
+// Explanation: 1, 3, and 4 occur exactly twice. 2 occurs exactly once. Hence the answer is 2.
+
+
+
+
+
+//Code --> Brute Force
+
+#include<vector>
+
+int getSingleElement(vector<int> &arr){
+	// Write your code here.	
+	int n = arr.size();
+
+	for (int i =0; i<n; i++){
+		int num = arr[i];
+		int count = 0;
+
+		for(int j = 0; j<n; j++){
+			if(arr[j] == num){
+				count++;
+			}
+		}
+
+		if(count == 1){
+			return num;
+		}
+
+	}
+
+	return -1;
+
+}
